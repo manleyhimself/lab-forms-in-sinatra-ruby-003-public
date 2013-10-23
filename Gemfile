a9cfd4e60f1b913eac64b88bc4a3892c5d04e66c
Spencer Rogers <spencer1248@gmail.com>
2013-10-23 14:44:15 -0400

    Add solution

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-reloader', require: false
 
+gem 'pry'
 gem 'sqlite3'
 gem 'sequel'
--- app.rb
+++ app.rb
@@ -11,5 +11,24 @@ module FormsLab
       @song = Song[params[:id]]
       erb :'songs/show'
     end
+
+    post '/songs/:id' do
+      song = Song[params[:id]]
+
+      song.update({
+        title: params[:title],
+        artist: params[:artist],
+        album: params[:album],
+        genre: params[:genre],
+        length: params[:length]
+      })
+
+      redirect "/songs/#{song.id}"
+    end
+
+    get '/songs/:id/edit' do
+      @song = Song[params[:id]]
+      erb :'songs/edit'
+    end
   end
 end
