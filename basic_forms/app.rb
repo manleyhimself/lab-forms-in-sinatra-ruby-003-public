1a7bac6e064c9deb7c1722df317b1355146cfaf7
manleyhimself <bcmanley1@gmail.com>
2013-10-23 21:19:24 -0400

    all done

--- basic_forms/app.rb
+++ basic_forms/app.rb
@@ -11,5 +11,22 @@ module FormsLab
       @song = Song[params[:id]]
       erb :'songs/show'
     end
+
+    get '/songs/:id/edit' do
+      @song = Song[params[:id]]
+      erb :'songs/edit'
+    end
+
+    post '/songs/:id/edit' do
+      @song = Song[params[:id]]
+      @song.artist = params[:name]
+      @song.title = params[:song_name]
+      @song.album = params[:album]
+      @song.genre = params[:genre]
+      @song.length = params[:song_length]
+      @song.save
+      redirect '/'
+    end
+
   end
 end
